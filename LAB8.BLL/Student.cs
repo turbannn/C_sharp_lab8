81e78b5422c32a040fbbd2dc94577d94d21ffbf5
George <George@DESKTOP-7ONPU8F>
2024-04-26 15:28:57 +0200

    Добавьте файлы проекта.

---
+++ LAB8.BLL/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB8.BLL
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Faculty { get; set; }
+        public int Index { get; set; }
+        public IList<double> Grades { get; set; }
+        public string GradesAsString 
+        { 
+            get
+            { 
+                if(Grades != null) 
+                    return string.Join(", ", Grades);
+
+                return "No Grades";
+            } 
+        }
+        public Student(string FName, string LName, string fac, int id)
+        {
+            FirstName = FName;
+            LastName = LName;
+            Faculty = fac;
+            Index = id;
+            Grades = new List<double>();
+        }
+        public Student() { }
+    }
+}
---
+++ LAB8.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LAB8.BLL;
+
+namespace LAB8.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IList<Student> Students { get; set; }
+        public MainWindow()
+        {
+            Students = new List<Student>()
+            {
+                new Student("Heorhii", "Mikula", "WIMII", 228666),
+                new Student("Jan", "Kowalski", "WIMII", 101010),
+                new Student("Michal", "Nowak", "WIMII", 221122),
+                new Student("Jacek", "Makieta", "WIMII", 332222)
+            };
+            foreach (var student in Students)
+            {
+                Random rnd = new Random();
+                student.Grades.Add(Math.Truncate((rnd.NextDouble() * 4 + 1) * 100) / 100);
+                student.Grades.Add(Math.Truncate((rnd.NextDouble() * 4 + 1) * 100) / 100);
+                student.Grades.Add(Math.Truncate((rnd.NextDouble() * 4 + 1) * 100) / 100);
+                student.Grades.Add(Math.Truncate((rnd.NextDouble() * 4 + 1) * 100) / 100);
+            }
+            InitializeComponent();
+            DataGridTable.ItemsSource = Students;
+            /*
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() { Header = "FirstName", Binding = new Binding(path: "FirstName") });
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() { Header = "LastName", Binding = new Binding(path: "LastName") });
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() { Header = "Faculty", Binding = new Binding(path: "Faculty") });
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() { Header = "Index", Binding = new Binding(path: "Index") });
+
+            DataGridTable.AutoGenerateColumns = false;
+            */
+
+        }
+        private void MainRemoveButtonStudentClick(object sender, RoutedEventArgs e)
+        {
+            if (DataGridTable.SelectedItem is Student std)
+            {
+                Students.Remove(std);
+                DataGridTable.Items.Refresh();
+            }
+        }
+        private void MainAddStudentButtonClick(object sender, RoutedEventArgs e)
+        {
+            AddStudentWindow addStudentWindow = new AddStudentWindow();
+            addStudentWindow.ShowDialog();
+            Students.Add(addStudentWindow.Student);
+            DataGridTable.Items.Refresh();
+        }
+        private void MainAddGradesToStudentClick(object sender, RoutedEventArgs e)
+        {
+            if (DataGridTable.SelectedItem is Student std)
+            {
+                AddGrade addGrade = new AddGrade();
+                addGrade.ShowDialog();
+                std.Grades.Add(addGrade.Value);
+                DataGridTable.Items.Refresh();
+            }
+        }
+    }
+}
